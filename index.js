e524de06ce16d2053aed00d7637cc176869b85ea
Lassi Oinas <lassi.oinas@hotmail.com>
2024-04-09 11:48:15 +0300

    3.7: phonebook-backend step7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const morgan = require("morgan")
 const app = express()
 
 let data =
@@ -26,6 +27,7 @@ let data =
   ]
 
 app.use(express.json())
+app.use(morgan("tiny"))
 
 app.get("/api/persons", (req, res) => {
   res.json(data)
