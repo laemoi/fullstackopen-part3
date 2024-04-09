b97aab338500bda5cbf8cdaf00d59770838b8450
Lassi Oinas <lassi.oinas@hotmail.com>
2024-04-09 13:37:19 +0300

    3.8*: phonebook-backend step8

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ let data =
   ]
 
 app.use(express.json())
-app.use(morgan("tiny"))
+const body = morgan.token("body", (req) => {
+  return JSON.stringify(req.body)
+})
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+
 
 app.get("/api/persons", (req, res) => {
   res.json(data)
