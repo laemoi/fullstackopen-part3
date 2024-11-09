cc01ba8e78ca794781121da1127ca59bea532ced
Lassi Oinas <lassi.oinas@hotmail.com>
2024-11-09 21:26:56 +0200

    3.17*: phonebook-database step 5

--- index.js
+++ index.js
@@ -62,6 +62,18 @@ app.post("/api/persons", (req, res) => {
   }
 })
 
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  Person
+    .findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => res.json(updatedPerson))
+    .catch(error => next(error))
+})
+
 app.get("/api/persons/:id", (req, res, next) => {
   Person
     .findById(req.params.id)
